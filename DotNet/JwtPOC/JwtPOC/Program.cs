d077fefb0015c943e085bce28d93d4cf837e39b6
Alan Tavares <alan1tavares@gmail.com>
2022-10-26 18:52:01 -0300

    Começado a poc sobre jwt

---
+++ DotNet/JwtPOC/JwtPOC/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+
+const string secretKey = "de1fde61701241c798fceca76b375bfd";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(op =>
+{
+    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGet("/ping", () =>
+{
+    return "pong";
+})
+.WithName("ping");
+
+app.MapGet("/secreteroute", () =>
+{
+    return "You find me";
+}).RequireAuthorization();
+
+app.Run();
---
+++ DotNet/JwtPOC/JwtPOC/README.md
@@ -0,0 +1,24 @@
+﻿# Pacotes
+
+Microsoft.AspNetCore.Authentication.JwtBearer
+
+Autenticaçao: determina a identidade do usário.
+Autorização: determina qual recurso o usuário tem acesso.
+
+Autenticação: manipulada pleo o serviço de autenticação ➡ `IAuthenticationService`.
+`IAuthenticationService`: é usado pelo o middleware de autenticação.
+
+Schemas: opções de configurações da atenticação.
+
+```c#
+builder.Services.AddAuthentication(schemas)
+	.AddJwtBearer(options);
+```
+
+`AddAuthentication`: define o tipo de schemas utilizado na autenticação.
+`AddJwtBearer`: utilizado para configurar o JWT.
+
+
+# Authetication scheme
+
+
