2c46111d08303d114e0a62d4ed0b68d6dac2a327
Alan Tavares <alan1tavares@gmail.com>
2023-09-01 16:50:07 -0300

    Verificando exceção com GUID

---
+++ DotNet/EmptyGuid/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Guid id  = Guid.Empty;
+
+Console.WriteLine(id);
+
+ArgumentNullException.ThrowIfNullOrEmpty(id.ToString());
+ArgumentNullException.ThrowIfNullOrEmpty(null);
