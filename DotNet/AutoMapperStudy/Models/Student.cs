0cf475702b4e44ad273732cb4922df879e10afef
Alan Tavares <alan1tavares@gmail.com>
2023-10-17 07:50:04 -0300

    feat: primeirso exemplo com AutoMapper

---
+++ DotNet/AutoMapperStudy/Models/Student.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapperStudy.Models
+{
+    public class Student
+    {
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public string? Address { get; set; }
+        public string? Department { get; set; }
+    }
+}
--- DotNet/AutoMapperStudy/Program.cs
+++ DotNet/AutoMapperStudy/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using AutoMapper;
+using AutoMapperStudy.Models;
+
 Console.WriteLine("Hello, World!");
+
+var config = new MapperConfiguration(config =>
+    config.CreateMap<Student, StudentDT>()
+);
+
+Student student = new Student
+{
+    Name = "Shivan",
+    Age = 20,
+    Address = "Bihar",
+    Department = "IT"
+};
+
+var mapper = new Mapper(config);
+var studentDT = mapper.Map<StudentDT>(student);
+
+Console.WriteLine($@"
+    Name: {studentDT.Name} 
+    Age: {studentDT.Age}
+    Addres: {studentDT.Address}
+    Department: {studentDT.Department} 
+");
