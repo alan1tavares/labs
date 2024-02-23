f4bd6601b9e85fc50b6203ecb1eab5f1c9185a94
Alan Tavares <alan1tavares@gmail.com>
2024-02-22 23:24:30 -0300

    Solução

--- react/Curso React Completo origamid/0301/src/App.tsx
+++ react/Curso React Completo origamid/0301/src/App.tsx
@@ -1,16 +1,46 @@
 import React from 'react'
+import './App.css'
+
+interface Item {
+  nome: string;
+  preco: number;
+}
 
 function App() {
+
+  const [dados, setDados] = React.useState<Item | null>(null);
+  const [isCarregando, setIsCarregando] = React.useState<boolean>(false);
+  const baseUrl = 'https://ranekapi.origamid.dev/json/api/produto/'
+
+  async function fetchData(produto: string) {
+    setIsCarregando(true);
+    const resp = await fetch(baseUrl + produto);
+    const json = await resp.json();
+    setDados(json);
+    setIsCarregando(false);
+  }
 
   return (
     <>
       <h1>
         Exercicio 0301
-        <button>notebook</button>
-        <button>smartphone</button>
-        <button>tablet</button>
       </h1>
-
+      <div style={{
+        display: 'flex',
+        gap: '2rem'
+      }}>
+        <button onClick={() => fetchData('notebook')}>notebook</button>
+        <button onClick={() => fetchData('smartphone')}>smartphone</button>
+        <button onClick={() => fetchData('tablet')}>tablet</button>
+      </div>
+      {
+        isCarregando ?
+          'Carregando' :
+          <div>
+            <p>{dados?.nome}</p>
+            <p>{dados?.preco}</p>
+          </div>
+      }
     </>
   )
 }
