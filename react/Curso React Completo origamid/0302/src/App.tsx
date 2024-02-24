4b77e09544b04b5282478d662a71c4279ff2a3d7
Alan Tavares <alan1tavares@gmail.com>
2024-02-23 21:41:59 -0300

    Solução do exercício 0302

--- react/Curso React Completo origamid/0302/src/App.tsx
+++ react/Curso React Completo origamid/0302/src/App.tsx
@@ -1,13 +1,62 @@
 
+import React from 'react';
 import './App.css'
 
+interface IProduto {
+  id: string;
+  nome: string;
+  preco: number;
+}
+
 function App() {
-  
+  const [dados, setDados] = React.useState<IProduto | null>(null);
+
+  React.useEffect(() => {
+    if (dados) {
+      localStorage.setItem('produto', dados.id);
+    }
+  }, [dados]);
+
+  React.useEffect(() => {
+    const produtoId = localStorage.getItem('produto');
+    produtoId && fetchProduto(produtoId);
+  }, []);
+
+  async function fetchProduto(nomeProduto: string) {
+    const baseUrl = 'https://ranekapi.origamid.dev/json/api/produto/';
+
+    const req = await fetch(baseUrl + nomeProduto);
+    const json = await req.json();
+    setDados(json);
+  }
+
   return (
     <>
+      <h1>Preferência: {dados && dados.id}</h1>
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <button onClick={() => fetchProduto('notebook')}>notebook</button>
+        <button onClick={() => fetchProduto('smartphone')}>smartphone</button>
+      </div>
 
+      {dados && <Produto produto={dados} />}
     </>
   )
 }
+
+interface ProdutoProps {
+  produto: IProduto
+}
+
+function Produto(props: ProdutoProps) {
+  const { produto } = props;
+  return (
+    <>
+      <h2>{produto.nome}</h2>
+      <p>Preço R$ {produto.preco}</p>
+    </>
+  )
+}
+
+
 
 export default App
