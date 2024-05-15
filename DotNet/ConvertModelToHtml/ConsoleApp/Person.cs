0912cab0d366117d4288efbc9fadd7d1abd8d66f
Alan Tavares <alan1tavares@gmail.com>
2024-05-15 19:47:49 -0300

    Exemplo incial

---
+++ DotNet/ConvertModelToHtml/ConsoleApp/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp;
+
+public class Person
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+    [HTMLFormType("TextArea")]
+    public string? Summary { get; set; }
+}
--- DotNet/ConvertModelToHtml/ConsoleApp/Program.cs
+++ DotNet/ConvertModelToHtml/ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
+
+TypeInfo typeInfo = typeof(Person).GetTypeInfo();
+
+Console.WriteLine("Properties");
+var props = typeInfo.DeclaredProperties;
+foreach (var prop in props)
+{
+    Console.WriteLine("Name " + prop.Name);
+    prop.GetCustomAttributes(typeof(HTMLFormTypeAttribute), true)
+        .Cast<HTMLFormTypeAttribute>().ToList().ForEach(att => Console.WriteLine("Attribute Name: " + att.Name));
+}
