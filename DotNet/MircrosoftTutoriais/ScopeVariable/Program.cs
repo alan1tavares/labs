6e864e9ad3f7273558d5b0575143f786818899ae
Alan Tavares <alan1tavares@gmail.com>
2024-06-03 21:32:28 -0300

    Estudo C# sobre escopo de variável

---
+++ DotNet/MircrosoftTutoriais/ScopeVariable/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Code sample 1
+bool flag = true;
+int value;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
